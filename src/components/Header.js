8de37e88fd6a7494c9017a46c79a758706b104af
visteen <visteen192@gmail.com>
2023-04-19 01:18:55 +0000

    fixing error

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,7 +16,7 @@ function Header() {
               <Nav.Link><i className = "fas fa-shopping-cart"></i>Cart</Nav.Link>
             </LinkContainer>
             <LinkContainer to='/login'>
-              <Nav.Link><i className = "fas fa-user"></i>Login</Nav.Link>
+              <Nav.Link ><i className = "fas fa-user"></i>Login</Nav.Link>
             </LinkContainer>
             
           </Nav>
--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -1,9 +1,15 @@
 import React from 'react'
-
+import { Link, useParams } from 'react-router-dom'
+import { Row,Col,Image,ListGroup,Button,Card } from 'react-bootstrap'
+import Rating from '../components/Rating'
+import products from '../products'
 const ProductPage = () => {
+    const params = useParams();
+    const id = params.id;
+    const product = products.find((p)=> p._id == id)
   return (
     <div>
-      this is the product page
+        {product.name}
     </div>
   )
 }
