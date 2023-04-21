aa279c39154e944bb1a500e2fe31815650542c10
visteen <visteen192@gmail.com>
2023-04-21 19:59:27 +0000

    product list constants created

--- src/Store.js
+++ src/Store.js
@@ -2,9 +2,11 @@ import { configureStore } from '@reduxjs/toolkit'
 import {legacy_createStore as createStore,combineReducers,applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import { productListReducer } from './reducers/ProductReducer'
 
-
-const reducer = combineReducers({})
+const reducer = combineReducers({
+    productList:productListReducer,
+})
 const initialState = {}
 const middleware = [thunk]
 const store = createStore(reducer,initialState,composeWithDevTools(applyMiddleware(... middleware)))
