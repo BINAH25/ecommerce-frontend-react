ac684edec6bb2a7ab4ea017a1d630ff501afe499
visteen <visteen192@gmail.com>
2023-04-23 00:19:37 +0000

    form container added

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -5,12 +5,23 @@ import { login } from '../action/UserActions'
 import Loader from '../components/Loader'
 import Message from '../components/Message'
 import {Row,Col,Form,Button} from 'react-bootstrap'
-
+import FormContainer from '../components/FormContainer'
 const LoginPage = () => {
+    const [user, setUser] = useState({
+        email: "",
+        password: "",
+    })
+    const handleChange = (event) => {
+        let name = event.target.name;
+        let value = event.target.value;
+    
+        setUser({ ...user, [name]: value });
+      };
+    
   return (
-    <div>
+    <FormContainer>
       this the login screen
-    </div>
+    </FormContainer>
   )
 }
 
